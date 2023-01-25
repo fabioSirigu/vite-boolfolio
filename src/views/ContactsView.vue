feb0ca0f2246b5b0e73d40e1467f04cdc3a936d7
naz1ne <fabio.sirigu.1995@gmail.com>
2023-01-25 13:37:10 +0100

    edit email form

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,6 +1,52 @@
 <script>
+import axios from 'axios'
+import { store } from '../store.js'
 export default {
-      name: 'ContactsView'
+      name: 'ContactsView',
+      data() {
+            return {
+                  store,
+                  name: '',
+                  email: '',
+                  message: '',
+                  success: false,
+                  loading: false,
+                  errors: {}
+            }
+      },
+      methods: {
+            sendForm() {
+                  this.loading = true;
+                  this.errors = {};
+
+                  /* console.log('name', this.name);
+                  console.log('email', this.email);
+                  console.log('message', this.message); */
+
+                  const data = {
+                        name: this.name,
+                        email: this.email,
+                        message: this.message
+                  }
+
+                  axios.post(`${this.store.api_base_url}/api/contacts`, data).then((response) => {
+
+                        this.success = response.data.success
+
+                        console.log(response);
+
+                        if (this.success) {
+                              this.name = ''
+                              this.email = ''
+                              this.message = ''
+                        }
+                        else {
+                              this.errors = response.data.errors
+                        }
+                        this.loading = false
+                  })
+            }
+      }
 }
 </script>
 
@@ -12,27 +58,38 @@ export default {
                   asperiores
                   sed est nulla esse quos quam laudantium, voluptatibus minus officiis assumenda. Ex vitae eos earum.
             </p>
-
-            <form action="">
+            <div v-if="success" class="alert alert-success text-start" role="alert">
+                  Messaggio inviato con successo!
+            </div>
+            <form @submit.prevent="sendForm()">
                   <div class="mb-3">
-                        <label for="" class="form-label">Full Name</label>
-                        <input type="text" name="" id="" class="form-control" placeholder="Mario Rossi"
-                              aria-describedby="fullNameHelper">
+                        <label for="name" class="form-label">Full Name</label>
+                        <input type="text" name="name" id="name" v-model="name" class="form-control"
+                              placeholder="Mario Rossi" aria-describedby="fullNameHelper">
+                        <p v-for="(error, index) in errors.name">
+                              {{ error }}</p>
                         <small id="fullNameHelper" class="text-muted">Add your full name</small>
                   </div>
                   <div class="mb-3">
-                        <label for="" class="form-label">Email</label>
-                        <input type="email" name="" id="" class="form-control" placeholder="mario.rossi@example.com"
-                              aria-describedby="emailHelper">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" name="email" id="email" v-model="email" class="form-control"
+                              placeholder="mario.rossi@example.com" aria-describedby="emailHelper">
+                        <p v-for="(error, index) in errors.email">
+                              {{ error }}</p>
                         <small id="emailHelper" class="text-muted">Add your email address</small>
                   </div>
 
                   <div class="mb-3">
-                        <label for="" class="form-label">Message</label>
-                        <textarea class="form-control" name="" id="" rows="5"></textarea>
+                        <label for="message" class="form-label">Message</label>
+                        <textarea class="form-control" name="message" id="message" v-model="message"
+                              rows="5"></textarea>
+                        <p v-for="(error, index) in errors.message">
+                              {{ error }}</p>
                   </div>
 
-                  <button type="submit" class="btn btn-primary">Contact Me</button>
+                  <button type="submit" class="btn btn-primary" :disabled="loading">
+                        {{ loading? 'Sending..': 'Contact me' }}
+                  </button>
             </form>
       </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,22 +14,10 @@ export default {
                               <img src="/img/user.jpg" alt="">
                         </div>
                         <div class="about_me">
-                              <h1>Home Page NazOne's Projects</h1>
-                              <h3 class="lead">
-                                    Banvenuto nella homepage del portfolio dei progetti di NazOne!
-                              </h3>
-                              <p class="description">Dopo varie esperienze lavorative nel mondo dei CND, con
-                                    specializzazioni negli
-                                    ultrasuoni, la
-                                    passione
-                                    per il coding ha preso il sopravvento dopo una vita intera che è rimasto fermo come
-                                    semplice
-                                    hobby.
-                                    <br>
-                                    L'avvento di Boolean ha dato una ventata d'aria fresca.
-                                    <br>
-                                    Qui troverai tutti i progetti e le tecnologie utilizzate nei progetti di NazOne.
-                              </p>
+                              <h1>NazOne's Projects</h1>
+                              <h2 class="description">Junior Full Stack Developer
+
+                              </h2>
                               <router-link :to="{ name: 'portfolio' }" class="project_link">
                                     All projects here!
                               </router-link>
@@ -47,7 +35,7 @@ export default {
 
       .layer {
             background-color: rgba($color: #000000, $alpha: 0.4);
-            padding: 2rem;
+            padding: 2rem 0;
             border-radius: 1rem;
 
             .img_user {
